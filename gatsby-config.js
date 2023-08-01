8bbfe2d41aed127b7c79483a74c51ce58e122c62
StasonJatham <machleidtk@gmail.com>
2023-08-01 20:39:43 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `1110-srlabs`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-sitemap"]
+};
